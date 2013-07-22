d4a6272b293f168e518c1ae467204c86da5b29f1
Samson Valland <samsonvalland@gmail.com>
2013-07-22 11:20:16 +0200

    Liten fiks i SesongerViewModel
    Put med id

--- src/main/webapp/js/viewModels/SesongerViewModel.js
+++ src/main/webapp/js/viewModels/SesongerViewModel.js
@@ -14,9 +14,9 @@ define([],function(){
 	return SesongerViewModel;
 });
 
-function putObject(koObject, controller){
+function putObject(koObject, controller){	
 	$.ajax({
-		url: CONTEXT_PATH + controller,
+		url: CONTEXT_PATH + controller + "/" + koObject.id(),
 		data: JSON.stringify(ko.mapping.toJS(koObject)),
 		type: "put",
 		dataType: 'json'
